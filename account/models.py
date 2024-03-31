78ff75b343708afa0c6ba4aede45701eaa475881
Rana Nouman <usman.latif.raw@gmail.com>
2024-03-31 23:53:25 +0500

    Customize Admin Panel

--- account/models.py
+++ account/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager,AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager,AbstractBaseUser, PermissionsMixin
 from enum import Enum
 from django.conf import settings  # Import settings to reference the AUTH_USER_MODEL
 #  Custom User Manager
@@ -36,7 +36,7 @@ class UserManager(BaseUserManager):
       return user
 
 #  Custom User Model
-class User(AbstractBaseUser):
+class User(AbstractBaseUser,PermissionsMixin):
   email = models.EmailField(
       verbose_name='Email',
       max_length=255,
@@ -54,6 +54,7 @@ class User(AbstractBaseUser):
   USERNAME_FIELD = 'email'
   REQUIRED_FIELDS = ['name', 'tc']
 
+  
   def __str__(self):
       return self.email
 
@@ -72,6 +73,13 @@ class User(AbstractBaseUser):
       "Is the user a member of staff?"
       # Simplest possible answer: All admins are staff
       return self.is_admin
+  
+  def get_all_permissions(self, obj=None):
+        if not self.is_active or self.is_anonymous:
+            return set()
+        if not hasattr(self, '_perm_cache'):
+            self._perm_cache = set(super().get_all_permissions(obj))
+        return self._perm_cache  
 
 
 
